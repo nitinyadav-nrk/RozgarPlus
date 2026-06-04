fa941a0dede4c20bc66fc6600c8c6fcba5136fe9
Nitin Yadav <nitinyadav.nrk@gmail.com>
2026-06-04 12:58:13 +0530

    Add health endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -29,6 +29,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("RozgarPlus API is running");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.use("/api", router);
 
 export default app;
